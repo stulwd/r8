0c84e890f7141715d39d9d6bd08eec0de97e0a27
stulwd <stulwd@163.com>
2018-09-19 14:17:38 +0800

    robotic arm control.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #define PS2_CMD PA7
 #define PS2_CS  PA4
 #define PS2_CLK PA5
+#define step 1
 
 Servo servo1;
 Servo servo2;
@@ -46,7 +47,7 @@ void setup() {
     pinMode(DIN1, OUTPUT);
     pinMode(DIN2, OUTPUT);
  
-   // pinMode(PWMA, OUTPUT);
+    pinMode(PWMA, OUTPUT);
     pinMode(PWMB, OUTPUT);
     pinMode(PWMC, OUTPUT);
     pinMode(PWMD, OUTPUT); 
@@ -71,81 +72,87 @@ void loop() {
     PS2::updatePS2();
 
 
+        // digitalWrite(AIN1,HIGH);
+        // digitalWrite(AIN2,LOW);
+        // digitalWrite(DIN1,HIGH);
+        // digitalWrite(DIN2,LOW);
+        // digitalWrite(BIN1,HIGH);
+        // digitalWrite(BIN2,LOW);
 
+        // digitalWrite(PWMA, HIGH);
+        // digitalWrite(PWMD, HIGH);
+        // digitalWrite(PWMB, HIGH);
     //motor control
+
     if(PS2::getButton(PS2Up))
     {
-        digitalWrite(AIN1, HIGH);
-        digitalWrite(AIN2, LOW);
+        digitalWrite(AIN1,HIGH);
+        digitalWrite(AIN2,LOW);
+        digitalWrite(DIN1,HIGH);
+        digitalWrite(DIN2,LOW);
 
-        digitalWrite(BIN1, HIGH);
-        digitalWrite(BIN2, LOW);
-
-        digitalWrite(CIN1, HIGH);
-        digitalWrite(CIN2, LOW);
-
-        digitalWrite(DIN1, HIGH);
-        digitalWrite(DIN2, LOW);
-
-        motor.write(180);
-       // digitalWrite(PWMA, HIGH);
-        digitalWrite(PWMB, HIGH);
-        digitalWrite(PWMC, HIGH);
-        digitalWrite(PWMD, HIGH);        
-
+        digitalWrite(PWMA, HIGH);
+        digitalWrite(PWMD, HIGH);
     }
-
-    if(PS2::getButton(PS2Down))
+    else if(PS2::getButton(PS2Down))
     {
-        digitalWrite(AIN2, HIGH);
-        digitalWrite(AIN1, LOW);
-
-        digitalWrite(BIN2, HIGH);
-        digitalWrite(BIN1, LOW);
+        digitalWrite(AIN2,HIGH);
+        digitalWrite(AIN1,LOW);
+        digitalWrite(DIN2,HIGH);
+        digitalWrite(DIN1,LOW);
         
-        digitalWrite(CIN2, HIGH);
-        digitalWrite(CIN1, LOW);
-        
-        digitalWrite(DIN2, HIGH);
-        digitalWrite(DIN1, LOW);
-
-        motor.write(0);
-        //digitalWrite(PWMA, HIGH);
-        digitalWrite(PWMB, HIGH);
-        digitalWrite(PWMC, HIGH);
-        digitalWrite(PWMD, HIGH);  
-
+        digitalWrite(PWMA, HIGH);
+        digitalWrite(PWMD, HIGH);
     }
+    else if(PS2::getButton(PS2Left))
+    {
+        digitalWrite(AIN2,HIGH);
+        digitalWrite(AIN1,LOW);
+        digitalWrite(DIN1,HIGH);
+        digitalWrite(DIN2,LOW);
 
-    if(PS2::getButton(PS2X))
+        digitalWrite(PWMA, HIGH);
+        digitalWrite(PWMD, HIGH);
+    }
+    else if(PS2::getButton(PS2Right))
+    {
+        digitalWrite(AIN1,HIGH);
+        digitalWrite(AIN2,LOW);
+        digitalWrite(DIN2,HIGH);
+        digitalWrite(DIN1,LOW);
+
+        digitalWrite(PWMA, HIGH);
+        digitalWrite(PWMD, HIGH);
+    }
+    else
     {
         digitalWrite(PWMA, LOW);
         digitalWrite(PWMB, LOW);
         digitalWrite(PWMC, LOW);
-        digitalWrite(PWMD, LOW);        
+        digitalWrite(PWMD, LOW);
     }
 
 
     //servo control
     if(PS2::getButton(PS2L1))
-    { pos1 += 2; cap(&pos1); servo1.write(pos1);}
+    { pos1 += step; cap(&pos1); servo1.write(pos1);}
     if(PS2::getButton(PS2L2))
-    { pos1 -= 2; cap(&pos1); servo1.write(pos1);}
+    { pos1 -= step; cap(&pos1); servo1.write(pos1);}
 
     if(PS2::getButton(PS2R1))
-    { pos4 += 2; cap(&pos4); servo4.write(pos4);}
+    { pos4 += step; cap(&pos4); servo4.write(pos4);}
     if(PS2::getButton(PS2R2))
-    { pos4 -= 2; cap(&pos4); servo4.write(pos4);}
+    { pos4 -= step; cap(&pos4); servo4.write(pos4);}
 
     if(PS2::getButton(PS2S))
-    { pos3 += 2; cap(&pos3); servo3.write(pos3);}
+    { pos3 += step; cap(&pos3); servo3.write(pos3);}
     if(PS2::getButton(PS2O))
-    { pos3 -= 2; cap(&pos3); servo3.write(pos3);}
+    { pos3 -= step; cap(&pos3); servo3.write(pos3);}
 
-    if(PS2::getButton(PS2Left))
-    { pos2 += 2; cap(&pos2); servo2.write(pos2);}
-    if(PS2::getButton(PS2Right))
-    { pos2 -= 2; cap(&pos2); servo2.write(pos2);}
+    if(PS2::getButton(PS2T))
+    { pos2 += step; cap(&pos2); servo2.write(pos2);}
+    if(PS2::getButton(PS2X))
+    { pos2 -= step; cap(&pos2); servo2.write(pos2);}
 
 
 
@@ -153,33 +160,7 @@ void loop() {
 
     delay(10);
     Serial.write('0');
-    Serial1.write(pos);
     Serial1.print('a');
-    if(pos == 180)
-    {
-        dir = 0;
-    }
-    if(pos == 0)
-    {
-        dir = 1;
-    }
-    if(dir == 1)
-    {
-        pos = pos + 5;
-        vol += 7;
-    }
-    if(dir == 0)
-    {
-        pos = pos - 5;
-        vol -= 7;
-    }
-    // digitalWrite(PWMA, HIGH);
-    // digitalWrite(PWMB, HIGH);
-    // digitalWrite(PWMC, HIGH);
-    // digitalWrite(PWMD, HIGH);
-
-
-
 }
 
 
